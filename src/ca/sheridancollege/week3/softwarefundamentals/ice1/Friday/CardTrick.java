089239ee9f72ccaaff703db65290d9fc60f075fa
goulbouq <92063165+goulbouq@users.noreply.github.com>
2021-10-09 04:57:51 -0400

    Added some code to print the values of the card created

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,11 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card Lebron = new Card();
+       Lebron.setSuit("Hearts");
+       Lebron.setValue(9);
+       System.out.println(Lebron.getSuit());
+       System.out.println(Lebron.getValue());
+       
     }
     
 }
